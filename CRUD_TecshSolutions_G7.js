ce18dfbcbcaa34753cd2da0e05741f06d8c895b5
arelytino <tl0479022012@unab.edu.sv>
2026-05-10 10:22:52 -0600

    Update CRUD_TecshSolutions_G7.js

--- CRUD_TecshSolutions_G7.js
+++ CRUD_TecshSolutions_G7.js
@@ -1,1 +1,25 @@
+// CRUD MongoDB - Feria de Logros 7° Grado - G7 TechSolutions
+use('TechSolutions_G7');
 
+// CREATE - Insertar 3 alumnos
+db.feria_logros_7grado.insertMany([
+  { nombre: "Alumno 1", grado: "7°", proyecto: "Robótica", nota: 95 },
+  { nombre: "Alumno 2", grado: "7°", proyecto: "Ciencias", nota: 88 },
+  { nombre: "Alumno 3", grado: "7°", proyecto: "Arte", nota: 92 }
+]);
+
+// READ - Consultar todos
+db.feria_logros_7grado.find();
+
+// UPDATE - Actualizar nota
+db.feria_logros_7grado.updateOne(
+  { nombre: "Alumno 1" }, 
+  { $set: { nota: 100 } }
+);
+
+// DELETE - Eliminar un registro
+db.feria_logros_7grado.deleteOne({ nombre: "Alumno 3" });
+
+
+3. Dale clic al botón verde: Confirma los cambios...
+4. Commit: Corrijo nombre a Tech y agrego código CRUD
